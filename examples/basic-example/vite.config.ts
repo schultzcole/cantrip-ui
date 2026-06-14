c25e8d5ccd1fc7319758f5cfe8570285b5dee64c
Cole <schultzcole@gmail.com>
2026-06-13 23:33:53 -0500

    examples: add vite bundle analyzer

--- examples/basic-example/vite.config.ts
+++ examples/basic-example/vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite"
 import deno from "@deno/vite-plugin"
+import { analyzer, unstableRolldownAdapter } from "vite-bundle-analyzer"
 
 export default defineConfig({
-    plugins: [deno()],
+    plugins: [
+        deno(),
+        unstableRolldownAdapter(analyzer({
+            summary: true,
+            analyzerMode: "static",
+            openAnalyzer: false,
+            fileName: "vite-bundle-stats",
+        })),
+    ],
 })
