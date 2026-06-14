2add67c01bb28d265a1e3f0e379dd41ae774a162
Cole <schultzcole@gmail.com>
2026-06-13 22:59:30 -0500

    examples: use cantrip ui

--- examples/basic-example/src/main.ts
+++ examples/basic-example/src/main.ts
@@ -5,7 +5,7 @@ import { temperatureConverter } from "./temperature-converter.ts"
 import { flightBooker } from "./flight-booker.ts"
 
 HtmlBuilder.build(document.getElementById("app")!, (_, { tag }) => {
-    tag("h1", (heading) => heading.replaceText("Demystica 7GUIs"))
+    tag("h1", (heading) => heading.html(`<em>cantrip</em> ui - 7GUIs`))
     tag<"div">("div", (col, { tag }) => {
         col.attrs({ className: "flex flex-col flex-gap" })
 
@@ -14,8 +14,8 @@ HtmlBuilder.build(document.getElementById("app")!, (_, { tag }) => {
                 .attrs({ className: "panel drop-shadow" })
                 .html(
                     `<p>
-                        A demo of Eugen Kiss's <a href="https://eugenkiss.github.io/7guis" target="_blank">7GUIs</a>
-                        UI framework benchmark implemented in Demystica.
+                        A demo of Eugen Kiss's <strong><a href="https://eugenkiss.github.io/7guis" target="_blank">7GUIs</a></strong>
+                        UI framework benchmark implemented in <strong><em>cantrip</em> ui</strong>.
                     </p>`,
                 )
         })
@@ -31,7 +31,7 @@ HtmlBuilder.build(document.getElementById("app")!, (_, { tag }) => {
             panel
                 .attrs({ className: "panel drop-shadow" })
                 .html(`<h2>Temperature Converter</h2>`)
-                .component(temperatureConverter, 0)
+                .component(temperatureConverter, 20)
         })
 
         tag("div", (panel) => {
